8573670c3d5e6868cfa9b6af6f933ea737811f36
danica00 <danicadujakovic00@gmail.com>
2022-10-11 15:18:18 +0200

    simple version

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = (Math.floor(Math.random()*6)+1);
+var randomNumber2 = (Math.floor(Math.random()*6)+1);
+var imageSource1 = "images/dice" +randomNumber1+".png";
+var imageSource2 = "images/dice" +randomNumber2+".png";
+document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
+document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
+if (randomNumber1>randomNumber2) {
+document.querySelector("h1").textContent="Player 1 won!";
+}else if (randomNumber2>randomNumber1) {
+document.querySelector("h1").textContent="Player 2 won!";
+}else {
+document.querySelector("h1").textContent="It's a draw";
+}
